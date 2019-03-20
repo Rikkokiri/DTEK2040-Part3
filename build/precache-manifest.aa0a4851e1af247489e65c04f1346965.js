43068d61b2dd00af576f32b9a0763657e229b34b
Rikkokiri <pilvi.rajala@gmail.com>
2019-03-20 11:27:31 +0200

    Added front-end build

---
+++ build/precache-manifest.aa0a4851e1af247489e65c04f1346965.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7a13ff25fbe6886b3a37",
+    "url": "/static/js/main.7a13ff25.chunk.js"
+  },
+  {
+    "revision": "627fd6acd35deffbc61b",
+    "url": "/static/js/2.627fd6ac.chunk.js"
+  },
+  {
+    "revision": "7a13ff25fbe6886b3a37",
+    "url": "/static/css/main.1ca742ce.chunk.css"
+  },
+  {
+    "revision": "086a8d243b6afe867af3aafa99d602b8",
+    "url": "/index.html"
+  }
+];
