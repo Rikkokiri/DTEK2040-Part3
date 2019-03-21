1cea335975983f6c45d52df5bc9411b53b560b83
Rikkokiri <pilvi.rajala@gmail.com>
2019-03-21 14:26:39 +0200

    Parts 3.11 and 3.12 done

--- index.js
+++ index.js
@@ -8,28 +8,6 @@ app.use(bodyParser.json())
 
 const Person = require('./models/person')
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Martti Tienari",
-    "number": "040-123456",
-    "id": 2
-  },
-  {
-    "name": "Arto Järvinen",
-    "number": "040-123456",
-    "id": 3
-  },
-  {
-    "name": "Lea Kutvonen",
-    "number": "040-123456",
-    "id": 4
-  }
-]
 
 app.get('/api/persons', (request, response) => {
   Person
@@ -37,23 +15,36 @@ app.get('/api/persons', (request, response) => {
     .then(persons => {
       response.json(persons.map(Person.format))
     })
+    .catch(error => {
+      console.log(error)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: "malformatted id" })
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  Person
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: "malformatted id" })
+    })
 })
 
 const generateId = () => {
@@ -61,10 +52,18 @@ const generateId = () => {
 }
 
 /*
+BEFORE:
 Implement error handling for the functionality adding new entries to the directory. The request should not be accepted if
 - name or number is missing from the request
 - the name to be added already exists in the directory
 Respond with an appropriate status code. Also include an informative error message */
+
+/*
+NOW:
+Change the backend so that the new entries are stored in the database.
+Here, you do not have to care about the fact that there may already be
+a person with the same name in the database.
+*/
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
@@ -76,18 +75,19 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({ error: 'number missing' })
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ error: 'name must be unique' })
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    id: generateId()
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-  response.json(person)
+  person
+    .save()
+    .then(newPerson => {
+      response.json(Person.format(newPerson))
+    })
+    .catch(error => {
+      console.log(error)
+    })
 })
 
 const PORT = process.env.PORT || 3001
